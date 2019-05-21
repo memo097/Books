9e9bd9d86a5a7f15a96beb384fd6defdb4bdee14
memo <memomemo@hotmail.com>
2019-05-21 11:35:17 +0200

    Function Based View Version

--- books/urls.py
+++ books/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
     path('edit/<int:pk>', views.BookUpdate.as_view(), name='book_edit'),
     path('delete/<int:pk>', views.BookDelete.as_view(), name='book_delete'),
+    path('', views.book_list, name='book_list'),
+    path('view/<int:pk>', views.book_view, name='book_view'),
+    path('new', views.book_create, name='book_new'),
+    path('edit/<int:pk>', views.book_update, name='book_edit'),
+    path('delete/<int:pk>', views.book_delete, name='book_delete'),
 ]
+
--- books/views.py
+++ books/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 
 from books.models import Book
 
@@ -24,3 +26,40 @@ class BookUpdate(UpdateView):
 class BookDelete(DeleteView):
     model = Book
     success_url = reverse_lazy('book_list')
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['name', 'pages']
+
+def book_list(request, template_name='books/book_list.html'):
+    book = Book.objects.all()
+    data = {}
+    data['object_list'] = book
+    return render(request, template_name, data)
+
+def book_view(request, pk, template_name='books/book_detail.html'):
+    book= get_object_or_404(Book, pk=pk)
+    return render(request, template_name, {'object':book})
+
+def book_create(request, template_name='books/book_form.html'):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, template_name, {'form':form})
+
+def book_update(request, pk, template_name='books/book_form.html'):
+    book= get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect('book_list')
+    return render(request, template_name, {'form':form})
+
+def book_delete(request, pk, template_name='books/book_confirm_delete.html'):
+    book= get_object_or_404(Book, pk=pk)
+    if request.method=='POST':
+        book.delete()
+        return redirect('book_list')
+    return render(request, template_name, {'object':book})
